60d5401dccbeee51c7a647e6b77e7b4d7be579ae
Henry Calderon <theceintiant@gmail.com>
2020-04-26 19:33:34 -0700

    Refactored the main. Added a git ignore file.

---
+++ Final-Newsstand-Project-HC/Controllers/NewsstandViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewsstandViewController.swift
+//  Final-Newsstand-Project-HC
+//
+//  Created by Henry Calderon on 4/26/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class NewsstandViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+//    func 
+
+}
